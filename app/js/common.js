b54650a27b589bb1db58ebb3e1f0028c6ac034dc
Dare4ka <chistiykhinadaria@gmail.com>
2018-07-02 19:05:06 +0300

    edited

--- app/js/common.js
+++ app/js/common.js
@@ -1,129 +1,77 @@
 $(document).ready(function(){
 
-		// слайдер в меню продуктов
-	function htmSlider(slideWrap, nextLink, prevLink, slideItem){
-		
-		// Клик по ссылке на следующий слайд 
-		nextLink.click(function(){
-			slideWrap.animate({left: slideWrap.position().left - $(slideItem).outerWidth()}, 500, function(){
-				slideWrap
-				.find(slideItem).first()
-				.appendTo(slideWrap)
-				.parent()
-				.css({'left': 0});
-			});
-		});
-
-		// Клик по ссылке на предыдующий слайд 
-		prevLink.click(function(){
-			slideWrap
-			.css({'left': slideWrap.position().left - $(slideItem).outerWidth()})
-			.find(slideItem).last()
-			.prependTo(slideWrap)
-			.parent()
-			.animate({left: 0}, 500);
-		});
+		//карусель продуктов
+	function carousel(block, arow) {
+		$(block).owlCarousel({
+  			nav: true,
+  			loop: true,
+  			dots: false,
+  			navContainer: arow,
+ 			responsive:{ 
+					0:{
+						items:1
+					},
+					420:{
+						items:2
+					},
+					680:{
+						items:3
+					},
+					880:{
+						items:4
+					}
+			}	
+  		});
 	};
- 
-		// иницилизация функции слайдера 
-	htmSlider($('.product__wrap_1'), $('.product__arow_next_1'), $('.product__arow_prev_1'), '.product__item_1');
-	htmSlider($('.product__wrap_2'), $('.product__arow_next_2'), $('.product__arow_prev_2'), '.product__item_2');
-	htmSlider($('.product__wrap_3'), $('.product__arow_next_3'), $('.product__arow_prev_3'), '.product__item_3');
-	htmSlider($('.product__wrap_4'), $('.product__arow_next_4'), $('.product__arow_prev_4'), '.product__item_4');
-	htmSlider($('.product__wrap_5'), $('.product__arow_next_5'), $('.product__arow_prev_5'), '.product__item_5');
+
+	carousel('#carousel-1', '#arow-1');
+	carousel('#carousel-2', '#arow-2');
+	carousel('#carousel-3', '#arow-3');
+	carousel('#carousel-4', '#arow-4');
+	carousel('#carousel-5', '#arow-5');
+
+		//карусель осковной слайдер
+	$('#slider').owlCarousel({
+  			nav: true,
+  			loop: true,
+  			autoplay: true,
+  			center: true,
+  			autoplayHoverPause: true,
+  			navContainer: '#slider-arow',
+  			dotsContainer: '#slider-btn',
+ 			responsive:{ 
+					0:{
+						items:1
+					}
+			}	
+  		});
+
 
 		//преключение слайдеров в меню
 	$('.menu__link_product').click(function openMenu(e) {
 		e.preventDefault();
-		$(this).toggleClass('menu__link_product_open')
-		.siblings('.product').toggleClass('product_open');
+		$(this).addClass('menu__link_product_open')
+		.siblings('.product').addClass('product_open');
 		$('.menu__link_product').not($(this))
 		.removeClass('menu__link_product_open')
 		.siblings('.product').removeClass('product_open');
 	});
+		//переключение меню навигации
+	$('.menu__link_navigation').click(function openNav(e) {
+		e.preventDefault();
+		$(this).toggleClass('menu__link_navigation_open')
+		.siblings('.menu__items_2').toggleClass('menu__items_2_open');
+		$('.menu__link_navigation').not($(this))
+		.removeClass('menu__link_navigation_open')
+		.siblings('.menu__items_2').removeClass('menu__items_2_open');
+	});
 
-		//основной не цикличный слайдер
-	function navigationSlider() {
-		var slideNow = 1;
-		var slideCount = $('.slider__wrap').children().length;
-		var slideInterval = 5000;
-		var navBtnId = 0;
-		var translateWidth = 0;
-		var switchInterval = setInterval(nextSlide, slideInterval);
-
-		$('.slider').hover(function() {
-			clearInterval(switchInterval);
-		}, function() {
-			switchInterval = setInterval(nextSlide, slideInterval);
-		});
-
-		$('.slider__arow_next_nav').click(function() {
-			nextSlide();
-		});
-
-		$('.slider__arow_prev_nav').click(function() {
-			prevSlide();
-		});
-		
-		$('.slider__btn').click(function() {
-			navBtnId = $(this).index();
-
-			if (navBtnId + 1 != slideNow) {
-				translateWidth = -$('.slider__list').width() * (navBtnId);
-				$('.slider__wrap').css({
-					'transform': 'translate(' + translateWidth + 'px, 0)',
-					'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-					'-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-				});
-			$(this).addClass('slider__btn_active');
-			$('.slider__btn').not($(this)).removeClass('slider__btn_active');
-			slideNow = navBtnId + 1;
-			}
-		});
-
-		function nextSlide() {
-			if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-				$('.slider__wrap').css('transform', 'translate(0, 0)');
-				slideNow = 1;
-			} else {
-				translateWidth = -$('.slider__list').width() * (slideNow);
-				$('.slider__wrap').css({
-					'transform': 'translate(' + translateWidth + 'px, 0)',
-					'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-					'-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-					});
-				slideNow++;
-			}
-		};
-
-		function prevSlide() {
-			if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
-				translateWidth = -$('.slider__list').width() * (slideCount - 1);
-				$('.slider__wrap').css({
-					'transform': 'translate(' + translateWidth + 'px, 0)',
-					'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-					'-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-				});
-				slideNow = slideCount;
-			} else {
-				translateWidth = -$('.slider__list').width() * (slideNow - 2);
-				$('.slider__wrap').css({
-					'transform': 'translate(' + translateWidth + 'px, 0)',
-					'-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-					'-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-				});
-				slideNow--;
-			}
-		};
-	};
-
-  navigationSlider();
-
+		//кнопка открытия адаптивного меню
 	$('.menu-open').click(function () {
 		$('.menu-open__global_top').toggleClass('menu-open__global_top_open');
 		$('.menu-open__global_middle').toggleClass('menu-open__global_middle_open');
 		$('.menu-open__global_bottom').toggleClass('menu-open__global_bottom_open');
 		$('.menu_navigation').toggleClass('menu_navigation_open');
-		$('.menu__item_navigation').slideToggle(200)
+		$('.menu__item_navigation').slideToggle(200);
 	});
 });
